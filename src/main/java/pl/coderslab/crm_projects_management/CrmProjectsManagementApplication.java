4444d3eb647ef5731a3c0b6437fbb3eb4a85cc43
MateuszStępkowski <stepkowski.mat@gmail.com>
2018-09-15 21:15:30 +0200

    base for project created by SpringBoot |&| Security Config created + properties for DB source added

---
+++ src/main/java/pl/coderslab/crm_projects_management/CrmProjectsManagementApplication.java
@@ -0,0 +1,12 @@
+package pl.coderslab.crm_projects_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmProjectsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmProjectsManagementApplication.class, args);
+	}
+}
